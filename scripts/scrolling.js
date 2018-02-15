5cf4b35ae4ffa63a29b1b196a735e9fb8dd72529
Reis Haleem <reishaleem@gmail.com>
2018-02-14 19:19:08 -0500

    updated to bootstrap 4

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -3,27 +3,11 @@ $(function() {
         $('.fill-screen').css('height', window.innerHeight);
     });
 
-    $('body').scrollspy({
-        target: '.navbar',
-        offset: 52
-    });
-
-    $('nav a, .down-arrow a').bind('click', function () {
+    $('.navbar-nav a, .down-arrow a, .fake-navbar a').bind('click', function () {
         $('html, body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 50
         }, 1000, 'easeInOutExpo');
         event.preventDefault();
-    });
-
-    $(window).scroll(function() {
-        if($(this).scrollTop() > $('#about').position().top-55)
-        {
-            $('.navbar-transparent').addClass('nonactive');
-            $('.nav-transparent').addClass('nonactive');
-        } else {
-            $('.navbar-transparent').removeClass('nonactive');
-            $('.nav-transparent').removeClass('nonactive');
-        }
     });
 
 });
