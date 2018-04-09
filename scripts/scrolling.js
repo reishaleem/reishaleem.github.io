2fe77da5b207cec2dc2c20abfec2e47242652ac4
Reis Haleem <reishaleem@gmail.com>
2018-04-08 21:00:42 -0400

    redesigned website

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -3,7 +3,7 @@ $(function() {
         $('.fill-screen').css('height', window.innerHeight);
     });
 
-    $('.navbar-nav a, .down-arrow a, .fake-navbar a').bind('click', function () {
+    $('.navbar-nav a, .down-arrow a, .sections a').bind('click', function () {
         $('html, body').stop().animate({
             scrollTop: $($(this).attr('href')).offset().top - 50
         }, 1000, 'easeInOutExpo');
